59812097b58274278509fc3c8277107106e27ded
Aaronaferns <aaronferns38@gamil.com>
2022-12-23 12:47:59 +0530

    Initial Project commit

---
+++ src/main/java/com/aaron/springboot/springpetclinic/AaronSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.aaron.springboot.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaronSpringPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaronSpringPetClinicApplication.class, args);
+	}
+
+}
